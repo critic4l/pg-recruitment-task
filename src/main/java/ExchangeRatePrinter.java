5d0ba8daa455c06f434603a60a01a2c7be112096
Artur Milancej <a.milancej@gmail.com>
2020-03-06 22:36:25 +0100

    removed counter from printer

--- src/main/java/ExchangeRatePrinter.java
+++ src/main/java/ExchangeRatePrinter.java
@@ -3,7 +3,6 @@ import java.util.List;
 public class ExchangeRatePrinter {
 
     public static void printExchangeRates(List<Rate> rates) {
-        int counter = 0;
         for (var rate : rates) {
             System.out.print(String.format("Kurs dla dnia %s: ask: %.4f, bid: %.4f", rate.getDate(), rate.getAsk(), rate.getBid()));
             System.out.print(String.format(", zmiana wzgledem poprzedniego dnia: ask: %.4f, bid: %.4f", rate.getAskChange(), rate.getBidChange()));
