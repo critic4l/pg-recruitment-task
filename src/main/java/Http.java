c2f7765db3322ea7bf98f21577acb2cca7408fe6
Artur Milancej <a.milancej@gmail.com>
2020-03-06 20:07:29 +0100

    Changed Java version to 11 to have access to better httpclient

--- src/main/java/Http.java
+++ src/main/java/Http.java
@@ -1,32 +1,23 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class Http {
 
     public void getData(String from, String to) throws IOException {
-        URL url = new URL("http://api.nbp.pl/api/exchangerates/rates/c/usd/" + from + "/" + to + "/");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        int responseCode = connection.getResponseCode();
-        if(responseCode == HttpURLConnection.HTTP_OK) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                    connection.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-            // print result
-            System.out.println(response.toString());
-        } else {
-            System.out.println("GET DIDN'T WORK!");
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create("http://api.nbp.pl/api/exchangerates/rates/c/usd/"+ from + "/" + to + "/"))
+                .GET()
+                .build();
+        try {
+            var response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
     }
 }
